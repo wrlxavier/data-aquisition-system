8e89c6f65305aa55b49769de017a6eb84700e11e
Andre  Lemos <andrepaim@gmail.com>
2023-05-11 23:39:17 +0000

    emulators

---
+++ emulators/client_emulator.py
@@ -0,0 +1,25 @@
+import argparse
+import socket
+
+def main(server_ip, server_port, sensor_id, num_records):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_ip, server_port))
+        request = f"{sensor_id}|{num_records}\r\n"
+        s.sendall(request.encode())
+        
+        print(f"Sent request: {request.strip()}")
+        
+        response = s.recv(1024).decode().strip()
+        print(f"Received response: {response}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Client emulator for data acquisition system.')
+    parser.add_argument('--ip', type=str, default='localhost',
+                        help='The IP address of the server.')
+    parser.add_argument('--port', type=int, default=9000,
+                        help='The port number of the server.')
+    parser.add_argument('sensor_id', type=str, help='The ID of the sensor.')
+    parser.add_argument('num_records', type=int, help='The number of records to request.')
+    
+    args = parser.parse_args()
+    main(args.ip, args.port, args.sensor_id, args.num_records)
---
+++ emulators/sensor_emulator.py
@@ -0,0 +1,34 @@
+import argparse
+import random
+import string
+import socket
+import time
+from datetime import datetime
+
+def random_string(length):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(length))
+
+def main(server_ip, server_port, sensor_id, frequency):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((server_ip, server_port))
+        while True:
+            value = random.uniform(-100, 100)
+            timestamp = datetime.now().isoformat()  # Generate current timestamp
+            message = f"{sensor_id}|{timestamp}|{value}\r\n"
+            s.sendall(message.encode())
+            time.sleep(frequency / 1000.0)  # Convert frequency from ms to s
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Sensor simulator for data acquisition system.')
+    parser.add_argument('--ip', type=str, default='localhost',
+                        help='The IP address of the server.')
+    parser.add_argument('--port', type=int, default=9000,
+                        help='The port number of the server.')
+    parser.add_argument('--sensor_id', type=str, default=random_string(10),
+                        help='The ID of the sensor.')
+    parser.add_argument('--frequency', type=int, default=1000,
+                        help='The frequency in milliseconds of sensor readings.')
+
+    args = parser.parse_args()
+    main(args.ip, args.port, args.sensor_id, args.frequency)
